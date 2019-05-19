c5d77c04295e3214c8025d0548f729aa957501f0
navigaid <navigaid@gmail.com>
2019-05-19 11:13:00 +0800

    write random uuid as header on connect

--- sea.go
+++ sea.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"github.com/google/uuid"
+	"io"
+	"log"
+	"net"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	log.Println("listening on port tcp://0.0.0.0:1337")
+	ln, err := net.Listen("tcp", ":1337")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		io.WriteString(conn, uuid.New().String())
+	}
 }
